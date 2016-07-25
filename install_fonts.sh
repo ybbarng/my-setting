1d2e88a976bc4804c2e3061f3d66c28c54d92587
ybbarng <ybbarng@gmail.com>
2016-07-25 18:31:31 +0900

    폰트 설치 스크립트 추가

---
+++ install_fonts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -pv ~/.fonts/UbuntuMono
+wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline%20Bold%20Italic.ttf
+wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline%20Bold.ttf
+wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline%20Italic.ttf
+wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf
