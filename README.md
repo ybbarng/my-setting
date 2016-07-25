af7bc51b838e67d58d4188b81e63448cb9111d46
ybbarng <ybbarng@gmail.com>
2016-07-25 18:05:11 +0900

    개발환경 설정용 스크립트 초안 작성

---
+++ README.md
@@ -0,0 +1,20 @@
+## A script for setting my terminal environment easily.
+
+
+### via wget
+```shell
+sh -c "$(wget https://raw.githubusercontent.com/ybbarng/my-setting/master/setting.sh -O -)"
+```
+
+### etc
+* oh-my-zsh를 설치하고 나면 새 세션이 실행되니 ctrl+d로 닫아줄 것
+
+## Powerline font 설정
+1. https://github.com/powerline/fonts 에서 원하는 폰트를 받음
+2. Powerline Font 설정하는 법 참고
+3. 우분투 : ~/.fonts에 폰트 파일을 넣고 fc-cache -vf ~/.fonts
+4. 폰트 설정
+
+
+### 주의 사항
+아직 테스트 해보지 않았음
---
+++ setting.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo 'Install prerequisites'
+sudo apt-get install git vim zsh wget tmux
+
+echo 'Install oh-my-zsh'
+#https://github.com/robbyrussell/oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+echo 'Install vim settings'
+git clone https://github.com/ybbarng/vim.git ~/.vim
+ln -sfv ~/.vim/rc/vimrc ~/.vimrc
+cd .vim
+git submodule init
+git submodule update
+cd bundle/Vundle.vim
+git checkout master
+git pull origin master
+vim +PluginUpdate +qall
+cd ~
+echo 'Install zsh syntax highlighing'
+git clone https://github.com/ybbarng/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
+echo 'Install zshrc and powerline theme'
+git clone https://github.com/ybbarng/zsh.git ~/.zsh
+ln -sfv ~/.zsh/zshrc.zsh ~/.zshrc.zsh
+ln -sfv ~/.zsh/powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
+echo 'Install zshrc and powerline theme'
+git clone https://github.com/ybbarng/tmux.git ~/.tmux
+ln -sfv ~/.tmux/tmux.conf ~/.tmux.conf
