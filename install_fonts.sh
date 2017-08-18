92dfb4fde86cb99c2db59473f91b432233f219b5
ybbarng <ybbarng@gmail.com>
2017-08-18 19:15:00 +0900

    폰트 캐시를 새로고침하도록 함

--- install_fonts.sh
+++ install_fonts.sh
@@ -5,3 +5,4 @@ wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/Ubuntu
 wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline%20Bold.ttf
 wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline%20Italic.ttf
 wget -P ~/.fonts/UbuntuMono https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf
+fc-cache -vf ~/.fonts
